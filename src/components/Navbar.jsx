2a9da98fd67aca91060a397861fdf232292e614f
Mohammad-akil <mohammadakilshakirhusain@gmail.com>
2023-05-04 13:02:28 +0530

    adding antdesign

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import { Button, Menu, Typography, Avatar } from "antd";
+import React from "react";
+import Link from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="nav-container">
+      <div className="logo-container"></div>
+    </div>
+  );
+};
+
+export default Navbar;
